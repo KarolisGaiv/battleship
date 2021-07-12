247f459c46882ef723ec9eff1f8934761bb549b3
KarolisGaiv <kgaivenis@gmail.com>
2021-07-12 21:30:29 +0300

    Init project structure

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/public/template.html",
+    }),
+  ],
+  devServer: {
+    contentBase: "./dist",
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
