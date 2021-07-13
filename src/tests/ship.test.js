1bf8d16216eb6b64331a48462b268d46fe09601b
KarolisGaiv <kgaivenis@gmail.com>
2021-07-13 19:12:49 +0300

    Add tests to Ship functionality

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,20 +1,76 @@
 import { expect, test } from "@jest/globals";
 import Ship from "../app/ship";
 
-describe("Ship Factory properties", () => {
-  test("Ship length is assigned to new ship objet", () => {
-    let newShipLength = 5;
-    const myNewShip = Ship(newShipLength);
-    expect(myNewShip.shipLength).toBe(newShipLength);
+describe("Ship Factory", () => {
+  describe("properties", () => {
+    test("Ship length is assigned to new ship objet", () => {
+      let newShipLength = 5;
+      const myNewShip = Ship(newShipLength);
+      expect(myNewShip.shipLength).toBe(newShipLength);
+    });
+
+    test("New ship is created with isDestroyed - false", () => {
+      const myNewShip = Ship(4);
+      expect(myNewShip.isDestroyed).toBe(false);
+    });
+
+    test("New ship has no damaged spots", () => {
+      const newShip = Ship(3);
+      expect(newShip.isShipBeenHit.length).toBe(0);
+    });
   });
 
-  test("New ship is created with isDestroyed - false", () => {
-    const myNewShip = Ship(4);
-    expect(myNewShip.isDestroyed).toBe(false);
+  describe("hit function", () => {
+    let newShip;
+    beforeEach(() => {
+      newShip = Ship(4);
+    });
+
+    test("no hits", () => {
+      expect(newShip.isShipBeenHit.length).toBe(0);
+    });
+
+    test("ship was not hit", () => {
+      newShip.hit(1);
+      expect(newShip.isShipBeenHit).toHaveLength(0);
+    });
+
+    test("ship was hit and is damaged", () => {
+      newShip.hit(3);
+      expect(newShip.isShipBeenHit).toHaveLength(1);
+    });
+
+    test("ship was succesfully hit more than once", () => {
+      newShip.hit(3);
+      newShip.hit(1);
+      newShip.hit(4);
+      expect(newShip.isShipBeenHit).toHaveLength(2);
+    });
   });
 
-  test("New ship has no damaged spots", () => {
-    const newShip = Ship(3);
-    expect(newShip.isShipBeenHit.length).toBe(0);
+  describe("isSunk functionality", () => {
+    let newShip;
+    beforeEach(() => {
+      newShip = Ship(4);
+    });
+
+    test("ship is alive", () => {
+      expect(newShip.isDestroyed).toBe(false);
+    });
+
+    test("ship is hit but not destroyed", () => {
+      newShip.hit(3);
+      newShip.isSunk();
+      expect(newShip.isDestroyed).toBe(false);
+    });
+
+    test("ship is hit and destroyed", () => {
+      newShip.hit(3);
+      newShip.hit(4);
+      newShip.hit(5);
+      newShip.hit(6);
+      newShip.isSunk();
+      expect(newShip.isDestroyed).toBe(true);
+    });
   });
 });
