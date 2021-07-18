81776cf58b0e79e4e8d61d436fc7c0bfe376e00e
KarolisGaiv <kgaivenis@gmail.com>
2021-07-18 13:48:27 +0300

    Add initial funcionality to display player and computer ship positions on boards

--- src/app/game.js
+++ src/app/game.js
@@ -6,35 +6,53 @@ function initiateGame() {
   const computer = Player("computer");
   const userGameboard = Gameboard();
   const computerGameboard = Gameboard();
-  console.log(userGameboard);
   userGameboard.placeShip(2, [3, 4]);
-  userGameboard.placeShip(3, [15, 16, 17]);
-  userGameboard.placeShip(4, [20, 21, 22, 23]);
+  userGameboard.placeShip(3, [53, 54, 55]);
+  userGameboard.placeShip(4, [34, 35, 36, 37]);
   userGameboard.placeShip(2, [10, 11]);
-  userGameboard.placeShip(2, [17, 18]);
+  userGameboard.placeShip(2, [83, 84]);
   computerGameboard.placeShip(2, [0, 1]);
   computerGameboard.placeShip(3, [7, 8, 9]);
   computerGameboard.placeShip(4, [15, 16, 17, 18]);
   computerGameboard.placeShip(2, [22, 23]);
   computerGameboard.placeShip(2, [4, 3]);
-  console.log(computerGameboard);
-  createGameboard();
+  createGameboard(user, userGameboard);
+  createGameboard(computer, computerGameboard);
 }
 
-function createGameboard() {
+function createGameboard(player, playerGameboard) {
+  // Create gameboard container
   const gameBoardWrapper = document.createElement("div");
   gameBoardWrapper.className = "gameBoard-wrapper";
   document.querySelector(".content").appendChild(gameBoardWrapper);
 
-  // Create and add squares to gameboard
+  // Create and add squares to gameboard container
+  const playerType = player.type;
   let n = 0;
   while (n < 100) {
     const square = document.createElement("div");
     square.className = "square";
-    square.setAttribute("id", n);
+    square.setAttribute("id", `${n}-${playerType}`);
     gameBoardWrapper.appendChild(square);
     n++;
   }
+  populatePlayerShips(playerType, playerGameboard);
+}
+
+function populatePlayerShips(playerType, playerGameboard) {
+  let playerShips = playerGameboard.shipsArray;
+  console.log(playerShips);
+
+  playerShips.forEach((ship) => {
+    const shipCordinates = ship.shipPosition;
+    console.log(shipCordinates);
+    shipCordinates.forEach((cordinate) => {
+      let squareToPopulate = document.getElementById(
+        `${cordinate}-${playerType}`
+      );
+      squareToPopulate.classList.add("-ship");
+    });
+  });
 }
 
 export { initiateGame };
